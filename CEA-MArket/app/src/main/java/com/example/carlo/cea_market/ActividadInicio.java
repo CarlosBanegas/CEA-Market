3297fd12d3cbb20c0c5f2d7b3bdfa7b1d90fb97b
Edgardo España <edgardo.espana@ujcv.edu.hn>
2016-04-08 23:01:27 -0600

    Actividad Inicio
    Esta actividad va ser la que se va a mostar al usuario despues de
    logearse

---
+++ CEA-MArket/app/src/main/java/com/example/carlo/cea_market/ActividadInicio.java
@@ -0,0 +1,13 @@
+package com.example.carlo.cea_market;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_inicio);
+    }
+}
